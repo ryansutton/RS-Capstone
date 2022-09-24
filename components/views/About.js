4ed2a39bebd25c78067e2baad28bc5d49516e2cd
ryansutton <ryanchristiansutton@gmail.com>
2022-09-23 23:11:24 -0500

    Fixed error in components/Nav.js that kept site from loading

--- components/views/About.js
+++ components/views/About.js
@@ -1,12 +1,10 @@
 import html from "html-literal";
 
 export default () => html`
-  <main>
-    <h2 id="aboutHeading">About</h2>
-    <p id="aboutParagraph">
-      STL Charity Connect is a site dedicated to help people in the St.Louis
-      area find charities to get involved with as well as connecting people
-      interested in getting involved in these local charities.
-    </p>
-  </main>
+  <h2 id="aboutHeading">About</h2>
+  <p id="aboutParagraph">
+    STL Charity Connect is a site dedicated to help people in the St.Louis area
+    find charities to get involved with as well as connecting people interested
+    in getting involved in these local charities.
+  </p>
 `;
--- store/Links.js
+++ store/Links.js
@@ -1,7 +1,5 @@
 export default [
   { title: "Home", text: "Home" },
   { title: "About", text: "About" },
-  { title: "Find a Charity", text: "Find a Charity" },
-  { title: "Community", text: "Community" },
   { title: "Contact", text: "Contact Us" }
 ];
