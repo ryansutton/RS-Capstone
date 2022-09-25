96e418ae5dd68bb4c0beef405703a23fa4ef072c
ryansutton <ryanchristiansutton@gmail.com>
2022-09-25 13:40:58 -0500

    Fix issue to have image display correctly and change CSS to center nav links after SPA conversion

--- store/Links.js
+++ store/Links.js
@@ -1,5 +1,7 @@
 export default [
   { title: "Home", text: "Home" },
+  { title: "Find a Charity", text: "Find a Charity" },
+  { title: "Community", text: "Community" },
   { title: "About", text: "About" },
   { title: "Contact", text: "Contact Us" }
 ];
