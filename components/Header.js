42d9e517929254a82e20ef5f6ef1ab1d45e6f98f
ryansutton <ryanchristiansutton@gmail.com>
2022-09-27 16:56:45 -0500

    edited header and footer

--- components/Header.js
+++ components/Header.js
@@ -2,6 +2,7 @@ import html from "html-literal";
 
 export default state => html`
   <header>
-    <h1>${state.header}</h1>
+    <!-- <h1>${state.header}</h1> -->
+    <h1>STL Charity Connect</h1>
   </header>
 `;
--- components/views/Home.js
+++ components/views/Home.js
@@ -1,5 +1,6 @@
 import html from "html-literal";
 import picture from "../../assets/img/pexels-julia-m-cameron-6994992.jpg";
+
 export default () => html`
   <img src=${picture} alt="Charity Image" />
 `;
